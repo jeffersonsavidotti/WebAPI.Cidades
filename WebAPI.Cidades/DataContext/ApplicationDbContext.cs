87f176900f64895401b6c290ed873989fd1ee615
Jefferson Aparecido Savidotti Dos Santos <jefferson.santos@programmers.com.br>
2024-02-05 20:54:55 -0300

    Adicionar arquivos de projeto.

---
+++ WebAPI.Cidades/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Cidades.Models;
+
+namespace WebAPI.Cidades.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CidadeModel> Cidades { get; set; }
+    }
+}
---
+++ WebAPI.Cidades/Models/CidadeModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Cidades.Models
+{
+    public class CidadeModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Estado { get; set;}
+
+    }
+}
